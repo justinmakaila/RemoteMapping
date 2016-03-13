8cc5dccf69c142f37dcb4d039e4b43c83307ff1d
Justin Makaila <justinmakaila@gmail.com>
2016-03-13 14:52:29 -0400

    Initial Commit

---
+++ CoreData+RemoteMappingTests/CoreData_RemoteMappingTests.swift
@@ -0,0 +1,36 @@
+//
+//  CoreData_RemoteMappingTests.swift
+//  CoreData+RemoteMappingTests
+//
+//  Created by Justin Makaila on 3/13/16.
+//  Copyright © 2016 justinmakaila. All rights reserved.
+//
+
+import XCTest
+@testable import CoreData_RemoteMapping
+
+class CoreData_RemoteMappingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
